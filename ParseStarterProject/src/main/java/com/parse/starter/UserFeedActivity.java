3de89418cab7d8a864f75aac0fd55f51eae604db
Nicole Tolch <tolch.nicole@gmail.com>
2020-04-02 14:02:29 -0500

    Retrieve user's images from Parse Server to display in user's feed

--- ParseStarterProject/src/main/java/com/parse/starter/UserFeedActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/UserFeedActivity.java
@@ -1,6 +1,8 @@
 package com.parse.starter;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +11,17 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 public class UserFeedActivity extends AppCompatActivity {
 
@@ -21,14 +34,44 @@ public class UserFeedActivity extends AppCompatActivity {
         String activeUsername = intent.getStringExtra("username");
         setTitle(activeUsername + "'s Feed");
 
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
-        ImageView imageView = new ImageView(getApplicationContext());
-        imageView.setLayoutParams(new LayoutParams(new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        )));
+        final LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
 
-        imageView.setImageDrawable(getResources().getDrawable(R.drawable.insta));
-        linearLayout.addView(imageView);
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Image");
+        query.whereEqualTo("username", activeUsername);
+        query.orderByDescending("createdAt");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> list, ParseException e) {
+                if (e == null) {
+                    if (list.size() > 0) {
+                        for (ParseObject object : list) {
+                            ParseFile file = (ParseFile) object.get("image");
+                            file.getDataInBackground(new GetDataCallback() {
+                                @Override
+                                public void done(byte[] bytes, ParseException e) {
+                                    if (e == null && bytes != null) {
+                                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                        ImageView imageView = new ImageView(getApplicationContext());
+                                        imageView.setLayoutParams(new LayoutParams(new ViewGroup.LayoutParams(
+                                                ViewGroup.LayoutParams.MATCH_PARENT,
+                                                ViewGroup.LayoutParams.WRAP_CONTENT
+                                        )));
+                                        imageView.setImageBitmap(bitmap);
+                                        linearLayout.addView(imageView);
+
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "No images", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
